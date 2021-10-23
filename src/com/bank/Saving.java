84667c624ea3a32f634f847258fe6ce36bd663bf
Ashish642 <88175180+Ashish642@users.noreply.github.com>
2021-10-23 07:44:04 +0530

    Update Saving.java

--- src/com/bank/Saving.java
+++ src/com/bank/Saving.java
@@ -3,8 +3,8 @@ package com.bank;
 public class Saving {
     public static void main(String[] args) {
 
-        BankingApplication person1 = new BankingApplication(8000);
-        BankingApplication person2 = new BankingApplication(5000);
+        BankingApplication sever1 = new BankingApplication(8000);
+        BankingApplication sever2 = new BankingApplication(5000);
         BankingApplication.modifyInterestRate(.02);
         person1.calculateMonthlyInterest();
         BankingApplication.modifyInterestRate(.02);
